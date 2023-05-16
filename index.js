168b3263f80f4d1f52bd95264d77ed971e0ef52c
Mohamed Akram <mohd.akram@outlook.com>
2023-05-16 04:27:04 +0400

    Improve formatter

---
+++ index.js
@@ -0,0 +1,178 @@
+const tagName = String.raw`[^/\s>]+`;
+
+const quotedAttrValue = String.raw`"(?<quotedAttrValue>[^"]*)"`;
+const singleQuotedAttrValue = String.raw`'(?<singleQuotedAttrValue>[^']*)'`;
+const unquotedAttrValue = String.raw`(?<unquotedAttrValue>[^\s>"']*)`;
+
+const attrValue = String.raw`${quotedAttrValue}|${singleQuotedAttrValue}|${unquotedAttrValue}`;
+const attr = String.raw`(?<attrSpace>\s+)(?<attrName>[^=\s>]+)(?:\s*=\s*(?:${attrValue}))?`;
+
+const tokens = {
+  comment: String.raw`<!--.*?-->`,
+  dtd: String.raw`<![^>]+>`,
+  startTag: String.raw`<\s*(?<startTagName>${tagName})(?<attrs>(?:${attr})*)\s*>`,
+  endTag: String.raw`<\s*/(?<endTagName>${tagName})\s*>`,
+  space: String.raw`\s+`,
+  text: String.raw`[^<\s]+`,
+  wildcard: String.raw`.+`,
+};
+
+/**
+ *
+ * @param {RegExp} lexer
+ * @param {string} s
+ */
+function* getTokens(lexer, s) {
+  let res;
+  let { lastIndex } = lexer;
+  while ((res = lexer.exec(s))) {
+    yield /** @type {RegExpExecArray & { groups: Record<string, string> }} */ (
+      res
+    );
+    ({ lastIndex } = lexer);
+  }
+  if (lastIndex != s.length) throw new Error("Failed to parse string");
+}
+
+const voidTags = new Set([
+  "area",
+  "base",
+  "br",
+  "col",
+  "command",
+  "embed",
+  "hr",
+  "img",
+  "input",
+  "keygen",
+  "link",
+  "menuitem",
+  "meta",
+  "param",
+  "source",
+  "track",
+  "wbr",
+]);
+
+function format(/** @type {string} */ html, indent = "  ", width = 80) {
+  const lexer = new RegExp(
+    Object.entries(tokens)
+      .map(([k, v]) => `(?<${k}>${v})`)
+      .join("|"),
+    "gys"
+  );
+  const attrLexer = new RegExp(attr, "gy");
+
+  /** @type {string[]} */
+  const output = [];
+
+  /** @type {string | null} */
+  let specialElement = null;
+  let level = 0;
+
+  let pendingIndent = false;
+  let lineLength = 0;
+
+  const addOutput = (/** @type {string[]} */ ...args) => {
+    for (const s of args) {
+      const pos = s.lastIndexOf("\n");
+      if (pos != -1) lineLength = s.length - pos - 1;
+      if (!specialElement) {
+        if (s == "\n") pendingIndent = true;
+        else {
+          if (lineLength + s.length > width) {
+            if (/^[ \t]+$/.test(s)) continue;
+            if (/^[ \t]+$/.test(output[output.length - 1])) output.pop();
+            addOutput("\n");
+          }
+          if (pendingIndent) {
+            const ind = indent.repeat(level);
+            output.push(ind);
+            pendingIndent = false;
+            lineLength += ind.length;
+          }
+          lineLength += s.length;
+        }
+      }
+      output.push(s);
+    }
+  };
+
+  for (const token of getTokens(lexer, html)) {
+    if (token.groups.endTag) {
+      const tagName = token.groups.endTagName.toLowerCase();
+      if (tagName == specialElement) specialElement = null;
+      if (!specialElement) {
+        --level;
+        addOutput(`</${tagName}>`);
+      }
+    }
+
+    if (!specialElement) {
+      if (token.groups.space) {
+        addOutput(...(token[0].match(/\n/g) ?? [" "]));
+      } else if (
+        token.groups.dtd ||
+        token.groups.comment ||
+        token.groups.wildcard
+      ) {
+        addOutput(token[0]);
+      } else if (token.groups.text) {
+        addOutput(token[0].replace(/[ \t]+/g, " "));
+      } else if (token.groups.startTag) {
+        const tagName = token.groups.startTagName.toLowerCase();
+
+        addOutput(`<${tagName}`);
+
+        if (["pre", "script", "style"].includes(tagName))
+          specialElement = tagName;
+
+        ++level;
+
+        if (token.groups.attrs) {
+          let { lastIndex } = attrLexer;
+          let attrToken;
+          while (
+            (attrToken =
+              /** @type {RegExpExecArray & { groups: Record<string, string> }} */ (
+                attrLexer.exec(token.groups.attrs)
+              ))
+          ) {
+            addOutput(
+              /\n/.test(attrToken.groups.attrSpace) ? "\n" : " ",
+              `${attrToken.groups.attrName}${
+                attrToken.groups.quotedAttrValue
+                  ? `="${attrToken.groups.quotedAttrValue}"`
+                  : attrToken.groups.singleQuotedAttrValue
+                  ? `='${attrToken.groups.singleQuotedAttrValue}'`
+                  : attrToken.groups.unquotedAttrValue
+                  ? `=${attrToken.groups.quotedAttrValue}`
+                  : ""
+              }`
+            );
+            ({ lastIndex } = attrLexer);
+          }
+          if (lastIndex != token.groups.attrs.length)
+            throw new Error("Failed to parse attributes");
+        }
+
+        addOutput(">");
+
+        if (voidTags.has(tagName)) --level;
+      }
+    } else addOutput(token[0]);
+  }
+
+  let newline = false;
+  while (/^\s+$/.test(output[output.length - 1])) {
+    const last = /** @type {string} */ (output.pop());
+    if (/\n/.test(last)) newline = true;
+  }
+
+  if (newline) output.push("\n");
+
+  return output.join("");
+}
+
+format.default = format;
+module.exports = format;
---
+++ test.js
@@ -0,0 +1,193 @@
+const nodeTest = require("node:test");
+
+const assert = require("assert/strict");
+
+const format = require("./index.js");
+
+/**
+ *
+ * @param {string} name
+ * @param {string} input
+ * @param {string} expected
+ * @param {string} [indent]
+ * @param {number} [width]
+ */
+function test(name, input, expected, indent, width) {
+  nodeTest.test(name, () => {
+    const actual = format(input, indent, width);
+    assert.equal(actual, expected);
+    if (width == undefined) width = 80;
+    for (const line of actual.split("\n"))
+      assert.ok(
+        line.length <= width,
+        `line.length = ${line.length} > ${width}`
+      );
+  });
+}
+
+test("No-op", "<body></body>", "<body></body>");
+
+test("Remove extra space 1", "<body>  </body>", "<body> </body>");
+test(
+  "Remove extra space 2",
+  "<body>   pasted  from   the internet   </body>",
+  "<body> pasted from the internet </body>"
+);
+
+test("Preserve newlines", "<body>\n\n</body>", "<body>\n\n</body>");
+
+test("Preserve trailing newline", "<body></body>\n\n\n", "<body></body>\n");
+
+test(
+  "Indent once",
+  "<body>\n<main></main></body>",
+  "<body>\n  <main></main></body>"
+);
+
+test(
+  "Indent twice",
+  "<body>\n<main>\n<div></div></main></body>",
+  "<body>\n  <main>\n    <div></div></main></body>"
+);
+
+test(
+  "Fix wrong indents",
+  "<body>\n <main>\n <div>\n  </div>\n   </main>\n </body>",
+  "<body>\n  <main>\n    <div>\n    </div>\n  </main>\n</body>"
+);
+
+test(
+  "Align attributes",
+  '<div id="container"\nclass="grid"></div>',
+  '<div id="container"\n  class="grid"></div>'
+);
+
+test("Remove tag space", "< div >< /div >", "<div></div>");
+
+test(
+  "Remove extra attribute space 1",
+  '<div id="container"  class="grid"></div>',
+  '<div id="container" class="grid"></div>'
+);
+test(
+  "Remove extra attribute space 2",
+  '<div id="container" class ="grid"></div>',
+  '<div id="container" class="grid"></div>'
+);
+test(
+  "Remove extra attribute space 3",
+  '<div id="container" class= "grid"></div>',
+  '<div id="container" class="grid"></div>'
+);
+test(
+  "Remove extra attribute space 4",
+  '<div id="container" class="grid" ></div>',
+  '<div id="container" class="grid"></div>'
+);
+test(
+  "Remove extra attribute space 5",
+  '<div  id = "container"  class = "grid" ></div>',
+  '<div id="container" class="grid"></div>'
+);
+
+test(
+  "Wrap many attributes",
+  '<input type="text" id="name" name="name" class="form-input" placeholder="Name" required>',
+  '<input type="text" id="name" name="name" class="form-input" placeholder="Name"\n  required>'
+);
+
+test(
+  "Wrap long line",
+  "<p>\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+  "<p>\n  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor\n  incididunt ut labore et dolore magna aliqua.</p>"
+);
+
+test(
+  "Do not format pre tag content",
+  '<pre  class= "code">  s  p  a  c  e  </pre>  s',
+  '<pre class="code">  s  p  a  c  e  </pre> s'
+);
+
+test(
+  "Do not format style tag content",
+  "<style>body {  }</style>",
+  "<style>body {  }</style>"
+);
+
+test(
+  "Do not format script tag content",
+  '<script  src="js/main.js">  console.log(  )  </script>',
+  '<script src="js/main.js">  console.log(  )  </script>'
+);
+
+test(
+  "Do not indent after doctype",
+  "<!doctype html>\n<html></html>",
+  "<!doctype html>\n<html></html>"
+);
+
+test(
+  "Do not indent after comment",
+  "<!-- useless comment -->\n<div></div>",
+  "<!-- useless comment -->\n<div></div>"
+);
+
+test(
+  "Do not parse tags inside special elements 1",
+  '<script>html = "</div>  </div>"</script><div>  </div>',
+  '<script>html = "</div>  </div>"</script><div> </div>'
+);
+test(
+  "Do not parse tags inside special elements 2",
+  "<!--< div>\n</div>  -->\n<div  ></div>",
+  "<!--< div>\n</div>  -->\n<div></div>"
+);
+
+test(
+  "Format element after empty comment",
+  "<!---->\n< div>  </div>",
+  "<!---->\n<div> </div>"
+);
+
+test(
+  "Do not remove space before double/triple equals 1",
+  `<ng-container *ngIf="formType === 'signup'">`,
+  `<ng-container *ngIf="formType === 'signup'">`
+);
+
+test(
+  "Do not remove space before double/triple equals 2",
+  `<ng-container *ngIf="formType !== 'signup'">`,
+  `<ng-container *ngIf="formType !== 'signup'">`
+);
+
+test(
+  "Different indent",
+  '<body>\n<main class="bg">   </main>\n</body>',
+  '<body>\n    <main class="bg"> </main>\n</body>',
+  " ".repeat(4)
+);
+
+test(
+  "Different indent and width",
+  '<body>\n<main class="bg">   </main>\n</body>',
+  '<body>\n    <main class="bg"\n        > </main>\n</body>',
+  " ".repeat(4),
+  20
+);
+
+test(
+  "Handle space around non-attribute quotes correctly",
+  '<a {{#if (equals pageLink "/users") }} class="is-active" {{/if}}>Users</a>',
+  '<a {{#if (equals pageLink "/users") }} class="is-active" {{/if}}>Users</a>'
+);
+
+test(
+  "Handle space inside non-attribute quotes correctly",
+  '<a {{#if (equals value " do not show ") }} hidden {{/if}}>Users</a>',
+  '<a {{#if (equals value " do not show ") }} hidden {{/if}}>Users</a>'
+);
+
+test("Handle invalid HTML", "<", "<");
+
+test("Void tags work correctly", "<br>\n<br>", "<br>\n<br>");
